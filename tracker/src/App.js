3273506beadd5186e9d006cfb4ba667c7b75ee93
Zicong Ma <zm1216@ic.ac.uk>
2018-05-31 13:52:41 +0100

    fixed prev commit

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -12,19 +12,8 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.socket = io.connect("https://www.jackpordi.com:443", {secure:true, reconnect:true, rejectUnauthorized: false});
+    this.socket = io.connect('https://www.jackpordi.com:443', { secure:true, reconnect:true, rejectUnauthorized: false });
   }
-	componentWillMount() {
-		const config = {
-			apiKey: 'AIzaSyANioEYzpzbNpXELf6CV7gkZW2JCPQRhtM',
-			authDomain: 'manager-fa7b3.firebaseapp.com',
-			databaseURL: 'https://manager-fa7b3.firebaseio.com',
-			projectId: 'manager-fa7b3',
-			storageBucket: 'manager-fa7b3.appspot.com',
-			messagingSenderId: '782159610243'
-		};
-		firebase.initializeApp(config);
-	}
 	// componentWillMount() {
 	// 	const config = {
 	// 		apiKey: 'AIzaSyANioEYzpzbNpXELf6CV7gkZW2JCPQRhtM',
