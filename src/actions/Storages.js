2b2691b60efb996eb2cd34d1dc43f89bdc095634
cassie-ying <xiaokage.ying@gmail.com>
2018-06-06 15:55:31 +0100

    Revert "Commented out testing code"
    This reverts commit 4a18b2f00681bfb73cde85b32ea845e896ea885c.

--- src/actions/Storages.js
+++ src/actions/Storages.js
@@ -3,61 +3,27 @@ import { AsyncStorage } from 'react-native';
 class Storages {
 
   static get(key) {
-    return AsyncStorage.getItem(key).then((value) => {
-      if (value) {
-        try {
-          value = JSON.parse(value);
-        } catch (error) {
-          console.log('get() error');
-        }
-      }
-      return value;
-    });
+      return AsyncStorage.getItem(key).then((value) => {
+          const jsonValue = JSON.parse(value);
+          return jsonValue;
+      });
   }
 
   static set(key, value) {
-    try {
       return AsyncStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.log('set() error');
-    }
   }
 
-  static delete(key) {
-    try {
-      return AsyncStorage.removeItem(key);
-    } catch (error) {
-      console.log('delete() error');
-    }
-  }
 
-  static getAllKeys() {
-    try {
-      return AsyncStorage.getAllKeys();
-    } catch (error) {
-      console.log('getAllKeys() error');
-    }
-  }
-
-  static clearAll() {
-    try {
-      return AsyncStorage.clear();
-    } catch (error) {
-      console.log('clearAll() error');
-    }
-  }
-
-  static update(key, value) {
-    return Storages.get(key).then((item) => {
-      try {
-        value = typeof value === 'string' ? value : Object.assign({}, item, value);
-        return AsyncStorage.setItem(key, JSON.stringify(value));
-      } catch (error) {
-        console.log('update() error');
-      }
-    });
-  }
-
+  // static update(key, value) {
+  //     return Storages.get(key).then((item) => {
+  //         value = typeof value === 'string' ? value : Object.assign({}, item, value);
+  //         return AsyncStorage.setItem(key, JSON.stringify(value));
+  //     });
+  // }
+  //
+  // static delete(key) {
+  //     return AsyncStorage.removeItem(key);
+  //   }
 }
 
 export default Storages;
