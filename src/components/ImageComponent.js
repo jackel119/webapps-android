140de8c07dd2845e9efaf902a8af6afab1712bde
Zicong Ma <mazicong@gmail.com>
2018-06-07 17:38:28 +0100

    Revert "addition of spinnning feature while waiting for feedback from server"
    This reverts commit 20fc5c813ad52dea9fd14d19eca7d764f464c494.

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,49 +1,14 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import { Image, Text, View } from 'react-native';
-import { Button, CardSection, Card, Spinner } from './common';
+import { Image } from 'react-native';
+import { Button, CardSection, Card } from './common';
 
 
 export default class ImageComponent extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      generating: false
-    };
-    console.log(this.state.generating);
-  }
-
   onButtonPress() {
-    this.setState({ generating: true });
-    // console.log(this.props);
+    console.log(this.props);
     // send and get back
-    // mock code here
-    setTimeout(() => { 
-      Actions.addTransaction({ scannedAmount: '8.40', scannedDate: '08/06/2018' });
-      this.setState({ generating: false }); 
-      }, 10000);
-  }
-
-  renderButton() {
-    if (this.state.generating) {
-      return <Spinner size="large" />;
-    }
-    return (
-      <Button onPress={this.onButtonPress.bind(this)}>
-        Generate Transaction
-      </Button>
-    );
-  }
-
-  renderText() {
-    if (this.state.generating) {
-      return (
-        <Text style={styles.textStyle}>
-          Generating!
-        </Text>
-      );
-    }
+    Actions.addTransaction({ scannedAmount: '8.40', scannedDate: '08/06/2018' });
   }
 
   render() {
@@ -58,23 +23,13 @@ export default class ImageComponent extends Component {
         </CardSection>
 
         <CardSection>
-          {this.renderText()}
-          {this.renderButton()}
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Generate Transaction
+          </Button>
         </CardSection>
 
       </Card>
     );
   }
 }
-
-const styles = {
-  textStyle: {
-    alignSelf: 'center',
-    color: '#ADD8E6',
-    fontSize: 16,
-    fontWeight: '600',
-    paddingTop: 10,
-    paddingBottom: 10
-  }
-};
 
