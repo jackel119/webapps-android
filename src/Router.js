4a14e5dcf8f71962685e5aef1f625cde72006e0c
cassie-ying <xiaokage.ying@gmail.com>
2018-06-07 21:00:41 +0100

    Add side menu, but menu button does not work

--- src/Router.js
+++ src/Router.js
@@ -1,49 +1,58 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Drawer } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import Homepage from './components/Homepage';
 import CameraComponent from './components/CameraComponent';
 import AddTransaction from './components/AddTransaction';
 import ImageComponent from './components/ImageComponent';
+import Setting from './components/Setting';
+
+import DrawerContent from './DrawerContent';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key="root" hideNavBar sceneStyle={{ paddingTop: 64 }}>
-        <Scene
-          key="login"
-          component={LoginForm}
-          title="Please Login"
-        />
+        <Drawer
+          hideNavBar
+          key="drawer"
+          contentComponent={DrawerContent}
+          drawerWidth={300}
+        >
+          <Scene initial key="main">
+            <Scene
+              initial
+              key="homepage"
+              component={Homepage}
+              title="Home"
+              onLeft="Menu"
+              hideNavBar
+            />
+            <Scene
+              key="addTransaction"
+              component={AddTransaction}
+              title="Add Transaction"
+            />
 
-        <Scene initial key="main">
-          <Scene
-            initial
-            key="homepage"
-            component={Homepage}
-            title="Home"
-            hideNavBar
-          />
-          <Scene
-            key="addTransaction"
-            component={AddTransaction}
-            title="Add Transaction"
-          />
+            <Scene
+              key="camera"
+              component={CameraComponent}
+              title="Scan Receipt"
+            />
 
-          <Scene
-            key="camera"
-            component={CameraComponent}
-            title="Scan Receipt"
-          />
+            <Scene
+              key="imageDisplay"
+              component={ImageComponent}
+              title="Scanned Image"
+            />
 
-          <Scene
-            key="imageDisplay"
-            component={ImageComponent}
-            title="Scanned Image"
-          />
-
-        </Scene>
-
+            <Scene
+              key="setting"
+              component={Setting}
+              title="Setting"
+            />
+          </Scene>
+        </Drawer>
       </Scene>
     </Router>
   );
