974eb00de57f74dacb7dd01e2b70fbe97c4c02ce
cassie-ying <xiaokage.ying@gmail.com>
2018-06-07 16:50:09 +0100

    add setting component

---
+++ src/components/Setting.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Setting extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Setting</Text>
+      </View>
+    );
+  }
+}
+
+export default Setting;
