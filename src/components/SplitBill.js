04e410f1a1198eaa0f285af570b22fd5c59878cf
Zicong Ma <mazicong@gmail.com>
2018-06-15 18:34:57 +0100

    split bill works quite nicely

--- src/components/SplitBill.js
+++ src/components/SplitBill.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, Picker } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+import Modal from 'react-native-modal';
 import MultiSelect from 'react-native-multiple-select';
-import { CardSection, Button } from './common';
+import { Button } from './common';
 
 
 class SplitBill extends Component {
@@ -11,9 +12,17 @@ class SplitBill extends Component {
 
     this.state = {
       selectedPeople: [],
-      splitEqually: false
+      splitEqually: false,
+      items: this.props.data
     };
-    this.items =
+
+    //update modalVisible for each item
+    var i;
+    for (i = 0; i < this.state.items.length; i++) {
+      this.setModalVisibility(i, false);
+    }
+
+    this.people =
       [{ id: '1',
         name: 'Jack',
       }, {
@@ -29,27 +38,43 @@ class SplitBill extends Component {
         id: '5',
         name: 'Zicong',
       }];
-    console.log(this.props);
   }
 
-  onSelectedItemsChange(selectedItems) {
-    this.setState({ selectedItems });
+  onSelectedItemsChange(selectedPeople) {
+    this.setState({ selectedPeople });
+    var i;
+    for (i = 0; i < this.state.items.length; i++) {
+      let temp = this.state.items.slice();
+      temp[i].people = selectedPeople;
+      this.setState({ items: temp });
+    }
+  }
+
+  onSelectedItemsChangeInner(selectedPeople, index) {
+    let temp = this.state.items.slice();
+    temp[index].people = selectedPeople;
+    this.setState({ items: temp });
+  }
+
+  setModalVisibility(index, visible) {
+    let temp = this.state.items.slice();
+    temp[index].modalVisible = visible;
+    this.setState({ items: temp });
   }
 
   renderTop() {
-    console.log(this.state.splitEqually);
     if (this.state.splitEqually) {
-      const { selectedItems } = this.state;
+      const { selectedPeople } = this.state;
       return (
       <View style={styles.topStyle}>
         <MultiSelect
           hideTags
           fixedHeight
-          items={this.items}
+          items={this.people}
           uniqueKey="id"
           ref={(component) => { this.multiSelect = component; }}
           onSelectedItemsChange={this.onSelectedItemsChange.bind(this)}
-          selectedItems={selectedItems}
+          selectedItems={selectedPeople}
           selectText="   Pick Friends"
           searchInputPlaceholderText="Search Friends..."
           tagRemoveIconColor="#CCC"
@@ -68,33 +93,86 @@ class SplitBill extends Component {
       return (
         <View style={styles.topStyle}>
           <Button onPress={() => { this.setState({ splitEqually: true }); }}>
-            AA?
+            Split Equally?
           </Button>
         </View>
       );
     }
   }
 
-  render() {
-    const renderData = this.props.data.map((data) => {
+  renderInnerSelect(index) {
+    const selectedPeople = this.state.items[index].people;
+    return (
+    <View style={styles.topStyle}>
+      <MultiSelect
+        hideTags
+        fixedHeight
+        items={this.people}
+        uniqueKey="id"
+        ref={(component) => { this.multiSelect = component; }}
+        onSelectedItemsChange={(selected) => this.onSelectedItemsChangeInner(selected, index)}
+        selectedItems={selectedPeople}
+        selectText="   Pick Friends"
+        searchInputPlaceholderText="Search Friends..."
+        tagRemoveIconColor="#CCC"
+        tagBorderColor="#CCC"
+        tagTextColor="#CCC"
+        selectedItemTextColor="#CCC"
+        selectedItemIconColor="#CCC"
+        itemTextColor="#000"
+        searchInputStyle={{ color: '#CCC' }}
+        submitButtonColor="#CCC"
+        submitButtonText="Submit"
+      />
+    </View>
+    );
+  }
+
+  renderSplit(id) {
+    if (this.state.splitEqually) {
       return (
-        <View style={styles.containerStyle}>
+        <Button onPress={() => this.setModalVisibility(id - 1, true)}>
+          Modify Split
+        </Button>
+      );
+    } 
+    return (
+      <Button onPress={() => this.setModalVisibility(id - 1, true)}>
+        Select People
+      </Button>
+    );
+  }
+
+  render() {
+    const renderData = this.props.data.map((data, index) => {
+      return (
+        <View style={styles.containerStyle} key={index}>
           <View style={styles.itemStyle}>
             <View style={{ flex: 1, flexDirection: 'row', alignItems: 'flex-end' }}>
               <Text style={{ fontSize: 16 }}> {data.id}. </Text>
               <Text style={{ fontSize: 16 }}> {data.name} </Text>
             </View>
-            <View style={{ justifyContent: 'flex-end' }}>
+            <View style={{ justifyContent: 'center' }}>
               <Text style={{ fontSize: 20 }}> {parseFloat(data.price).toFixed(2)} </Text>
             </View>
           </View>
           <View style={styles.shareStyle}>
-            <Text style={styles.labelStyle}>shared with</Text>
-            <Picker style={styles.inputStyle}>
-              <Picker.Item label="Jack1" value='1' />
-              <Picker.Item label="Jack2" value='2' />
-            </Picker>
+            {this.renderSplit(data.id)}
           </View>
+          <Modal 
+            isVisible={this.state.items[data.id - 1].modalVisible}
+            backdropOpacity={0.5}
+          >
+            <View style={{ flex: 1 }}>
+              <Text>{'Item: ' + data.name + '  ' + 'Price: ' + data.price }</Text>
+              {this.renderInnerSelect(data.id - 1)}
+              <Button
+                onPress={() => this.setModalVisibility(data.id - 1, false)}
+              >
+                <Text>Complete Split</Text>
+              </Button>
+            </View>
+          </Modal>
         </View>
       );
     });
@@ -112,7 +190,7 @@ class SplitBill extends Component {
           </View>
         </ScrollView>
         <View style={{ flex: 0.1 }}>
-          <Button>
+          <Button onPress={() => console.log(this.state)}>
             Submit
           </Button>
         </View>
