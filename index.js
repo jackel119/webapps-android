bd6259d1d06b8bc9e58cfefd8ccdbf55fec840be
Zicong Ma <mazicong@gmail.com>
2018-06-18 13:53:50 +0100

    split bill changes

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
 
 //ignore isMountedWarning
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 console.disableYellowBox = true;
 AppRegistry.registerComponent('tracker', () => App);
